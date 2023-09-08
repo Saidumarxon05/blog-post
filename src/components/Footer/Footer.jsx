bd91506981544d83695f82ec844edfdfb957984a
Saidumarxon05 <saidumarxon39@gmail.com>
2023-09-08 18:31:57 +0500

    initial commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,42 @@
 import cls from './styles.module.scss'
+import Logo from '../assets/images/Logo.svg'
+
 
 export const Footer = () => {
-  return <div className={cls.footer}>Footer</div>
+  return (
+    <footer className={cls.footer}>
+            <div className={cls.container}>
+                <div className={cls.logoWrap}>
+                <img width='103.5px' height='34.5px'  className={cls.footerLogo} src={Logo} alt='FooterLogo' />
+            <p className={cls.footerContent}>Помощник в публикации статей, журналов.<br/> 
+               Список популярных международных конференций.<br/>
+                Всё для студентов и преподавателей.
+                    </p>
+                    <p className={cls.copyright}>Copyright © 2020. LogoIpsum. All rights reserved.</p>
+                </div>
+   
+            <ul className={cls.recourses}>
+                    <li>Ресурсы</li>
+                    <li>Статьи</li>
+                    <li>Журналы</li>
+                    <li>Газеты</li>
+                    <li>Диплом</li>
+                </ul>
+
+            <ul className={cls.about}>
+                <li>О нас</li>
+                <li>Контакты</li>
+                <li>Помощь</li>
+                <li>Заявки</li>
+                <li>Политика</li>
+            </ul>
+
+            <ul className={cls.help}>
+                <li>Помощь</li>
+                <li>Часто задаваемые вопросы</li>
+            </ul>
+            </div>
+    </footer>
+  )
+
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,36 @@
 import { Link } from 'react-router-dom'
 import cls from './styles.module.scss'
+import Logo from '../assets/images/Logo.svg'
+
 
 export const Header = () => {
-  return <header className={cls.header }>
-    <nav>
-      <ul>
-        <li>
+  return (
+    <header className={cls.header}>
+    <div className={cls.container}>
+        {/* <nav>
+            <ul>
+              <li>
           <Link to="/todos">Todos</Link>
-        </li>
-      </ul>
-    </nav>
+             </li>
+            </ul>
+        </nav> */}
+        <img src={Logo} alt='Logo' />
+        <ul className={cls.contentList}>
+          <li>Все потоки</li>
+          <li>Разработка</li>
+          <li>Администрирование</li>
+          <li>Дизайн</li>
+          <li>Маркетинг</li>
+          <li>Все потоки</li>
+          <li>Научпоп</li>
+        </ul>
+                <div className={cls.wrapper}>
+                <button className={cls.btn}>Войти</button>
+                </div>
+            </div>
+
+
   </header>
+  )
+
 }
